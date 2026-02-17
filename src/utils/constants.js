01bb93f9be879d3e7d1f5dd73c2f677db72dcc9a
ImSachin023 <sachinabx3@gmail.com>
2026-02-17 18:06:22 +0530

    change on constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   location.hostname === "localhost"
     ? "http://localhost:7777"
-    : "https://dev-coder-backend.onrender.com";
+    : "https://dev-coder-backend.vercel.app";
